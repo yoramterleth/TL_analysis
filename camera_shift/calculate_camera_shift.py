e7767eaec08e4d5ac35a534fca11ffbcb79333f4
liellnima <julia.kaltenborn@mail.mcgill.ca>
2025-08-18 20:36:06 +0200

    add decomposition of homography and extracting angles

--- camera_shift/calculate_camera_shift.py
+++ camera_shift/calculate_camera_shift.py
@@ -1,3 +1,5 @@
+import math
+from scipy.spatial.transform import Rotation
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
@@ -6,20 +8,56 @@ from matplotlib import pyplot as plt
 MIN_MATCH_COUNT = 10
 DISTANCE_RATIO_LOWE = 0.3
 
+# camera params
+FOCAL_MM = 34
+SENSOR_X_MM = 22.3
+SENSOR_Y_MM = 14.9
+
 class CameraShiftCalculator():
     """ Class that is used to calculate the camera shifts (yawn angle) between two images.
     """
-    def __init__(self, refer_img: np.ndarray, shift_img: np.ndarray):
+    def __init__(
+            self, 
+            refer_img: np.ndarray, 
+            shift_img: np.ndarray,
+            focal_mm: float, 
+            sensor_x_mm: float, 
+            sensor_y_mm: float,
+
+        ):
         """ Initialize the class by providing two images
 
         Args:
-            refer_img (np.ndarray): Reference image with known yawn angle
-            shift_img (np.ndarray): Shifted image for which we want to know the new yawn angle
+            refer_img : Reference image with known yawn angle
+            shift_img: Shifted image for which we want to know the new yawn angle
+            focal_mm: focal length of camera in mm
+            sensor_x_mm: pixel size of sensor along x-axis (width)
+            sensor_y_mm: pixel size of sensor along y-axis (height)
         """
+        # attributes set from the beginning
         self.refer_img = refer_img 
         self.shift_img = shift_img
-        
-        # TODO consider turning into gray scale images for better sifting
+        self.focal_mm = focal_mm                # taken from project_tracked_features
+        self.sensor_x_mm = sensor_x_mm          # pixel size on sensor, along x-axis
+        self.sensor_y_mm = sensor_y_mm          # pixel size on sensor, along y-axis
+        self.cx = self.refer_img.shape[1] / 2   # half of width to get img center
+        self.cy = self.refer_img.shape[0] / 2   # half of height to get img center
+        self.fx = (self.focal_mm / self.sensor_x_mm) * self.refer_img.shape[1]
+        self.fy = (self.focal_mm / self.sensor_y_mm) * self.refer_img.shape[0]
+
+        # attributes set during usage
+        self._H = None 
+        self._mask = None
+        self._good_matches = None
+        self._reference_points = None
+        self._shifted_points = None 
+        self._reference_kp = None 
+        self._shifted_kp = None
+
+        # the angles that can be returned in the end
+        self.roll_deg = None
+        self.pitch_deg = None 
+        self.yaw_deg = None 
 
     def show_images(self):
         """ Shows reference and shifted image
@@ -31,17 +69,17 @@ class CameraShiftCalculator():
     def get_sift_features(self, min_match_count: int = 10, distance_ratio_lowe: float = 0.3) -> dict:
         """ Retrieve SIFT features and store only good features.
 
-        Returns: dict:
-                good_matches: All the matches
-                reference_points: Source points of reference image
-                shifted_points: Destination points of shifted image
-                reference_kp: Keypoints of the reference image 
-                shifted_kp: Keypoints of the shifted image
-
         Args:
-            min_match_count (int): Minimum number of good features that must be found.
-            distance_ratio_lowe (float): Ratio between the best match and second best match.
+            min_match_count: Minimum number of good features that must be found.
+            distance_ratio_lowe: Ratio between the best match and second best match.
                 The smaller the more conservative in finding good matches. Consider setting to 0.5 or lower.
+
+                Sets:
+                self.good_matches: All the matches
+                self.reference_points: Source points of reference image
+                self.shifted_points: Destination points of shifted image
+                self.reference_kp: Keypoints of the reference image 
+                self.shifted_kp: Keypoints of the shifted image
         
         Raises:
             RuntimeError if not enough good matches were found
@@ -76,48 +114,114 @@ class CameraShiftCalculator():
         pts_ref = np.float32([ kp_ref[m.queryIdx].pt for m in good_matches ]).reshape(-1,1,2)
         pts_shi = np.float32([ kp_shi[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
 
-        return {
-            "good_matches": good_matches,
-            "reference_points": pts_ref,
-            "shifted_points": pts_shi,
-            "reference_kp": kp_ref,
-            "shifted_kp": kp_shi,
-        }
+        # set attributes for class
+        self._good_matches = good_matches 
+        self._reference_points = pts_ref 
+        self._shifted_points = pts_shi 
+        self._reference_kp = kp_ref 
+        self._shifted_kp = kp_shi
     
-    def find_homography(self, good_matches: list, reference_points: np.ndarray, shifted_points: np.ndarray, reference_kp: tuple, shifted_kp: tuple):
+    def find_homography(self):
+        """ Find the homography 
+        Sets:
+                np.ndarray: The homography
+                np.ndarray: The mask
+        """
+        self._H, self._mask = cv.findHomography(self._reference_points, self._shifted_points)
+    
+    def draw_matched_features(self):
         """ Find the homography and visualize the matched keypoints and the overlap.
+
         """
         # find homography
-        M, mask = cv.findHomography(reference_points, shifted_points)
-        matchesMask = mask.ravel().tolist()
+        matchesMask = self._mask.ravel().tolist()
 
         # find perspective transformations
         h,w,c = self.refer_img.shape
         pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv.perspectiveTransform(pts,M)
+        dst = cv.perspectiveTransform(pts,self._H)
 
         # drawing the box
         self.shifted_img = cv.polylines(self.shift_img,[np.int32(dst)],True,255,3, cv.LINE_AA)
 
         # draw matched points
-        draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
-        matched_img = cv.drawMatches(self.refer_img, reference_kp, self.shift_img, shifted_kp, good_matches, None,**draw_params)
+        draw_params = dict(
+            matchColor = (0,255,0), # draw matches in green color
+            singlePointColor = None,
+            matchesMask = matchesMask, # draw only inliers
+            flags = 2,
+            )
+        matched_img = cv.drawMatches(self.refer_img, self._reference_kp, self.shift_img, self._shifted_kp, self._good_matches, None,**draw_params)
         
         plt.imshow(matched_img)
         plt.show()
 
+    def calc_rotation_matrix(self):
+        """ Calculates the angles between reference and shifted image
+        """
+        ## Trial 1
+
+        # # perform singular value decomposition
+        # U, _, Vt = np.linalg.svd(self._H) # U: left singular vectors; _: singular vectors (unused), Vt: right singular vectors transposed
+        
+        # # rotation matrix is the vector cross product of U and Vt
+        # R = U @ Vt 
+
+        # # get angles from rotation matrix
+        # # see e.g. here: https://web.archive.org/web/20220428033032/http://planning.cs.uiuc.edu/node103.html
+        # yaw = math.atan2(R[1][0], R[0][0]) 
+        # pitch = - math.asin(R[2][0])# OR: atan2 (-R20 / sqrt(R21**2 + R22**2))
+        # roll = math.atan2(R[2][1], R[2][2])
+
+        # # convert to degrees and assign to class attributes
+        # self.yaw_deg = math.degrees(yaw)
+        # self.pitch_deg = math.degrees(pitch)
+        # self.roll_deg = math.degrees(roll)
+
+        # print(self.yaw_deg)
+        # print(self.pitch_deg)
+        # print(self.roll_deg)
+        # exit(0)
+
+        ## Trial 2
+
+        # arrange camera intrinsic parameters as camera matrix
+        K = np.array([[self.fx, 0, self.cx], [0, self.fy, self.cy], [0, 0, 1]])
+
+        # decompose homography matrix (I tried singular value decomposition before, not sure what cv2 is doing under the hood)
+        _, rotations, translations, normals = cv.decomposeHomographyMat(self._H, K)
+
+        # extract one solution (missing step here: deriving the best solution??), as there are several ones
+        R = rotations[0]
+
+        # use scipy to get the Euler angles (exact same results like using atan2 and converting to degrees afterwards)
+        self.roll_deg, self.pitch_deg, self.yaw_deg = Rotation.from_matrix(R).as_euler("xyz", degrees=True)
+
+
+    def get_angles(self) -> tuple:
+        """ Returns how the image has been shifted
+        Returns:
+            tuple:
+                float: roll angle
+                float: pitch angle
+                float: yaw angle
+        """
+        return self.roll_deg, self.pitch_deg, self.yaw_deg
 
 if __name__ == "__main__":
 
     refer_img = cv.imread("data/reference_pic.png")
     shift_img = cv.imread("data/shifted_pic.png")
 
-    calc = CameraShiftCalculator(refer_img, shift_img)
+    calc = CameraShiftCalculator(
+        refer_img, 
+        shift_img,
+        FOCAL_MM,
+        SENSOR_X_MM,
+        SENSOR_Y_MM,
+    )
 
-    # look at our original pics
+    # visualize our original pics
     #calc.show_images()
 
     # find sift features and find best matches
@@ -126,5 +230,16 @@ if __name__ == "__main__":
         distance_ratio_lowe=DISTANCE_RATIO_LOWE,
     )
 
-    # find homography and visualize our matched sift features
-    calc.find_homography(**sift_dict)
+    # find homography 
+    calc.find_homography()
+
+    # visualize our matched sift features
+    #calc.draw_matched_features()
+
+    # find yaw angle via rotation map
+    calc.calc_rotation_matrix()
+    roll, pitch, yaw = calc.get_angles()
+    print("The image has shifted with the following angles:")
+    print("     Roll Deg:", roll)
+    print("     Pitch Deg:", pitch)
+    print("     Yaw Deg:", yaw)
